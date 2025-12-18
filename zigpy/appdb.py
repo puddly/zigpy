d42ef95c9d97c5b10c003ff3849ae0b0a140b888
puddly <32534428+puddly@users.noreply.github.com>
2025-12-18 13:19:11 -0500

    Account for newer versions of aiosqlite moving away from being a Thread subclass

--- zigpy/appdb.py
+++ zigpy/appdb.py
@@ -201,7 +201,10 @@ class PersistingListener(zigpy.util.CatchingTaskMixin):
         await self._db.close()
 
         # FIXME: aiosqlite's thread won't always be closed immediately
-        await asyncio.get_running_loop().run_in_executor(None, self._db.join)
+        await asyncio.get_running_loop().run_in_executor(
+            None,
+            (self._db.join if hasattr(self._db, "join") else self._db._thread.join),
+        )
 
     def enqueue(self, cb_name: str, *args) -> None:
         """Enqueue an async callback handler action."""
